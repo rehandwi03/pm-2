2693edea3a1a7d1b61755f01da6d8aac1f1a0e95
rehandwi03 <rehandwi0395@gmail.com>
2022-03-22 01:13:52 +0700

    tugas 3

--- lib/main.dart
+++ lib/main.dart
@@ -27,13 +27,56 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Webview"),
-        ),
-        body: WebView(
-          initialUrl: "https://flutter.dev",
-          javascriptMode: JavascriptMode.unrestricted,
-        ));
+    return WillPopScope(
+      onWillPop: () => showExitPopup(context),
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Task 3"),
+          ),
+          body: Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+            margin: EdgeInsets.all(20),
+            child: WebView(
+              initialUrl: "https://rehandwi03.github.io",
+              javascriptMode: JavascriptMode.unrestricted,
+            ),
+          )),
+    );
+  }
+
+  Future<bool> showExitPopup(BuildContext context) async {
+    return await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Container(
+              height: 90,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Do you want to exit?"),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("Yes"),
+                      )),
+                      SizedBox(width: 15),
+                      Expanded(
+                          child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("No"),
+                      ))
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
