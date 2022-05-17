c38a5eb8250183a051eb061516a6c72aa8e32b46
rehandwi03 <rehandwi0395@gmail.com>
2022-05-18 03:17:10 +0700

    init provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:pm_2/qr-scan.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-Widget button(BuildContext context, String url, btnText, IconData icon) {
-  return TextButton(
-      onPressed: () async {
-        if (!await launch(url)) throw 'Could not launch';
-      },
-      child: Container(
-        child: Column(
-          children: [
-            Icon(
-              icon,
-              size: 80,
-            ),
-            Text(btnText)
-          ],
-        ),
-      ));
-}
-
 class Home extends StatelessWidget {
-  String googleUrl =
-      'https://www.google.com/maps/dir/?api=1&destination=72.817622,19.123799';
-
   Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -35,55 +12,7 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text("Pertemuan 5"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                button(context, "https://flutter.dev", "Website",
-                    Icons.open_in_browser),
-                button(
-                    context,
-                    'mailto:rehandwi2910@gmail.com?subject=This is Subject Title&body=This is Body of Email',
-                    "Email",
-                    Icons.email)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                button(context, "tel:+6281317588508", "Phone", Icons.phone),
-                button(
-                    context, 'sms:+6281317588508?body=Halo', "SMS", Icons.sms)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                button(context, googleUrl, "Location", Icons.location_on),
-                TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const QRScan()));
-                    },
-                    child: Column(
-                      children: [
-                        Icon(
-                          Icons.qr_code,
-                          size: 80,
-                        ),
-                        Text("Scan")
-                      ],
-                    ))
-              ],
-            )
-          ],
-        ),
-      ),
+      body: Center(child: Text("Hello World")),
     );
   }
 }
